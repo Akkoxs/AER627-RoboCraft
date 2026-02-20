f406efe12f74afacb8ed16ac372be03d5cce27c4
Kai <kai.stewart.eng@gmail.com>
2026-02-19 19:14:07 -0500

    started hopper programming

--- Project2/src/main.cpp
+++ Project2/src/main.cpp
@@ -6,19 +6,59 @@
 /*    Description:  EXP project                                               */
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
+
+//PORTS
+//PORT5 = VERTICAL PRISMATIC
+//PORT1 = CLAW 
+//PORT2 = OPTICAL SENSOR
+//PORT3 = HORIZONTAL ROTARY 
+//PORT6 = HOPPER 
+
+
+
+
 #include "vex.h"
 
 using namespace vex;
 
-// A global instance of vex::brain used for printing to the EXP brain screen
-vex::brain       Brain;
+//general
+brain Brain;
+controller Controller = controller();
+float pi = 3.14159265358979323846264; 
+float rads2Deg = pi/180;
 
-// define your global instances of motors and other devices here
+//state flags 
+bool newBlock;
 
+//hopper
+motor hopMotor = motor(PORT6, false);
+int hopStep = 0;
+float hopAngDisp = 0.0;
+float hopAngStep = 90.0;
+
+void StartUp(){
+    hopMotor.setPosition(0, degrees);
+
+}
+
+void ZeroMotor(motor& Motor, bool& homingFlag){
+    Motor.stop();
+    Motor.setPosition(0, degrees);
+    homingFlag = true;
+}
+
+void DispenseBlock(){
+    hopStep++;
+    
+    while (hopAngDisp!= 90*hopStep){
+        hopMotor.spin(vex::forward);
+    }
+        
+}
 
 int main() {
 
-    Brain.Screen.printAt( 2, 30, "Hello EXP" );
+    StartUp();
 
     while(1) {
        
@@ -27,6 +67,8 @@ int main() {
     }
 
 }
+
+
 
 
 
