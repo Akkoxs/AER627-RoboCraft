63fd4f153dbe3d82604ab87f5be01f71c45948f4
Kai <kai.stewart.eng@gmail.com>
2026-02-25 02:55:19 -0500

    started working on state machine implementation and Startup()

--- Project2/src/main.cpp
+++ Project2/src/main.cpp
@@ -14,9 +14,6 @@
 //PORT3 = HORIZONTAL ROTARY 
 //PORT6 = HOPPER 
 
-
-
-
 #include "vex.h"
 
 using namespace vex;
@@ -24,11 +21,53 @@ using namespace vex;
 //general
 brain Brain;
 controller Controller = controller();
+optical Optical = optical(PORT2);
 float pi = 3.14159265358979323846264; 
 float rads2Deg = pi/180;
 
-//state flags 
+//state machine
+enum State {STARTUP, 
+            DISPENSE_BLOCK, 
+            TRAVERSE_TO_PICKUP_ZONE,
+            DESCEND,
+            GRAB, 
+            ASCEND, 
+            TRAVERSE_TO_RED_ZONE,
+            TRAVERSE_TO_BLUE_ZONE,
+            RELEASE, 
+            END};
+
+//array of pointers to an array of constant chars
+const char* stateNames[] = {
+    "STARTUP",
+    "DISPENSE_BLOCK",
+    "TRAVERSE_TO_PICKUP_ZONE",
+    "DESCEND",
+    "GRAB",
+    "ASCEND",
+    "TRAVERSE_TO_RED_ZONE",
+    "TRAVERSE_TO_BLUE_ZONE",
+    "RELEASE",
+    "END"};
+
+State currentState;
 bool newBlock;
+
+//optical sensor
+color detectedColor;
+//Optical.objectLost(func);
+
+
+//gantry
+motor horizontalMotor = motor(PORT3, false);
+bool horizontalHomingFlag = false;
+
+//claw
+motor verticalMotor = motor(PORT5, false);
+bool verticalHomingFlag = false;
+
+motor clawMotor = motor(PORT1, false);
+
 
 //hopper
 motor hopMotor = motor(PORT6, false);
@@ -36,10 +75,74 @@ int hopStep = 0;
 float hopAngDisp = 0.0;
 float hopAngStep = 90.0;
 
-void StartUp(){
-    hopMotor.setPosition(0, degrees);
+//misc 
 
+
+
+void StartUp(){
+    UpdateScreen();
+    hopMotor.setPosition(0, degrees);
+    horizontalMotor.setPosition(10, degrees);
+    verticalMotor.setPosition(10, degrees);
+
+    //ZeroMotor(horizontalMotor, )
 }
+
+void UpdateScreen(){
+    Brain.Screen.clearScreen();
+    Brain.Screen.setFont(mono12);
+    Brain.Screen.printAt(55, 45, "CURRENT STATE");
+    Brain.Screen.printAt(55, 55, stateNames[currentState]);
+}
+
+void EvaluateState(State state){
+    switch (state){
+        case 0: //STARTUP 
+            //
+            break;
+        
+        case 1: //DISPENSE BLOCK
+            //
+            break;
+
+        case 2: //TRAVERSE TO PICKUP ZONE
+            //
+            break;
+
+        case 3: //DESCEND
+            //
+            break;
+
+        case 4: //GRAB
+            //
+            break;
+
+        case 5: //ASCEND
+            //
+            break;
+
+        case 6: //TRAVERSE TO RED ZONE
+            //
+            break;
+         
+        case 7: //TRAVERSE TO BLUE ZONE
+            //
+            break;
+
+        case 8: //RELEASE
+            //
+            break;
+
+        case 9: //END
+            //
+            break;
+
+        default: 
+            //
+            break;
+    }
+}
+
 
 void ZeroMotor(motor& Motor, bool& homingFlag){
     Motor.stop();
@@ -60,10 +163,17 @@ int main() {
 
     StartUp();
 
-    while(1) {
-       
-        // Allow other tasks to run
-        this_thread::sleep_for(10);
+
+    while(true) {
+        
+        EvaluateState(currentState);
+        UpdateScreen();
+
+
+
+
+        
+        wait(20, msec);
     }
 
 }
